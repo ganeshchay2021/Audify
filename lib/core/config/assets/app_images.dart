9e1ccf05de85ab55d3e0bf514449f05d8f4d97b5
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-04-05 19:06:51 +0545

    Feat(Assets): App image and app vector widget added

---
+++ lib/core/config/assets/app_images.dart
@@ -0,0 +1,10 @@
+class AppImages {
+  //basePath and format
+  static const String basePath = "assets/images/";
+  static const String format = ".png";
+
+  //images paths
+  static const String introBG = "${basePath}introBG$format";
+  static const String introBG2 = "${basePath}introBG2$format";
+
+}
---
+++ lib/core/config/assets/app_vectors.dart
@@ -0,0 +1,8 @@
+class AppVectors {
+  //vectors basepath and format
+  static const String basePath = "assets/vectors/";
+  static const String format = ".svg";
+
+  //vectors paths
+  static const String appLogo = "${basePath}audify_logo.png";
+}
