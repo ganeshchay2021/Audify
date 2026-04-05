aca5dfee2a9688fe00d5530c5e27705741038522
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-04-05 19:10:50 +0545

    Feat(Main): Root File Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:audify/core/config/theme/app_theme.dart';
+import 'package:audify/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:audify/presentation/splash/page/splash_view.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorageDirectory.web
+        : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
   runApp(const MyApp());
 }
 
@@ -11,10 +22,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: AppTheme.lightTheme,
-      home: SplashView(),
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => ThemeCubit())],
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, mode) => MaterialApp(
+          title: 'Audify',
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.lightTheme,
+          darkTheme:AppTheme.darkTheme,
+          themeMode: mode,
+          home: SplashView(),
+        ),
+      ),
     );
   }
 }
