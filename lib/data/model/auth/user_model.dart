bb7a1782d832680670038f99c0943ed85944840e
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-04-08 19:35:14 +0545

    Feat(Model): UserModel Added

---
+++ lib/data/model/auth/user_model.dart
@@ -0,0 +1,12 @@
+class UserModel {
+  final String? fullName;
+  final String email;
+  final String password;
+
+  UserModel({
+    this.fullName,
+    required this.email,
+    required this.password,
+  });
+  
+}
