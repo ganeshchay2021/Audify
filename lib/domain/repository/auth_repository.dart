7ba111932d0fab555143662813e22dd88cc9e641
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-04-08 19:38:02 +0545

    Feat(Domain Repository): Auth Repo Added

---
+++ lib/domain/repository/auth_repository.dart
@@ -0,0 +1,7 @@
+import 'package:audify/data/model/auth/user_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class AuthRepository {
+  Future<Either> signUp(UserModel userModel);
+  Future<Either> signIn(UserModel userModel);
+}
